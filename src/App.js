0bb85581fef3aad6963ff1a37b8a0374bb8f55eb
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-03 06:30:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <>
@@ -18,7 +19,14 @@ function App() {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <input type="text" />
+        <input
+          className=""
+          type="password"
+          autoComplete="current-password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </form>
     </>
   );
