c20df20fae11af1d845b6f237c20e250f71787a8
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-03 06:49:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import { useState } from "react";
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      if (username === "Gerges" && password === "1995") {
+        setLoggedIn(true);
+      } else {
+        throw Error;
+      }
+    } catch (error) {
+      setError("incorrect username or password");
+    }
+  };
 
   return (
     <>
@@ -10,9 +25,12 @@ function App() {
         useReducer Simple Login Form
       </h1>
 
-      <form className="">
+      <form
+        className="flex flex-col items-center justify-center gap-4 mt-4 py-2"
+        onSubmit={handleSubmit}
+      >
         <input
-          className=""
+          className="border rounded-lg px-2 py-1"
           type="text"
           autoComplete="username"
           placeholder="Username"
@@ -20,13 +38,21 @@ function App() {
           onChange={(e) => setUsername(e.target.value)}
         />
         <input
-          className=""
+          className="border rounded-lg px-2 py-1"
           type="password"
           autoComplete="current-password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+
+        <button
+          className="bg-blue-500 text-white text-lg 
+        font-medium rounded-lg py-1 px-3"
+          type="submit"
+        >
+          Log in
+        </button>
       </form>
     </>
   );
