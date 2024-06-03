d6305e578080bfd893f62cbdb99c3ced59d16905
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-03 06:27:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [username, setUsername] = useState("");
+
   return (
-    <div className="App">
-      <h1 className="">useReducer Simple Login Form</h1>
-    </div>
+    <>
+      <h1 className="text-center font-semibold text-3xl mt-2 py-4">
+        useReducer Simple Login Form
+      </h1>
+
+      <form className="">
+        <input
+          className=""
+          type="text"
+          autoComplete="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input type="text" />
+      </form>
+    </>
   );
 }
 
